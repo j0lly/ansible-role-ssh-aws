d664c8cb3861e47ce3eee540d16797931e2630a6
j0lly <jos.ferraris@gmail.com>
2017-02-09 23:30:54 +0100

    moved to docker (can be tested with travis); added sshd & cron checks

--- tests/test_epel.py
+++ tests/test_epel.py
@@ -1,3 +1,9 @@
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
 def test_epel_is_installed(Package):
     epel = Package('epel-release')
     assert epel.is_installed
--- tests/test_pip.py
+++ tests/test_pip.py
@@ -1,19 +1,17 @@
-import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
 
 
-@pytest.fixture()
-def AnsibleDistribution(Ansible):
-    """ Grab Os specific values """
-    return Ansible("setup")["ansible_facts"]["ansible_distribution"]
-
-
-def test_pip_is_installed(Package, AnsibleDistribution):
+def test_pip_is_installed(Package, SystemInfo):
     # This is sub-optimal, need better way to parametrize
-    if AnsibleDistribution == 'CentOS':
+    if SystemInfo.distribution == 'centos':
         pip = Package('python2-pip')
-    elif AnsibleDistribution == 'Amazon':
+    elif SystemInfo.distribution == 'amzn':
         pip = Package('python27-pip')
     else:
-        raise ValueError("Unsupported distribution: " + AnsibleDistribution)
+        raise ValueError(
+                "Unsupported distribution: " + SystemInfo.distribution)
 
     assert pip.is_installed
