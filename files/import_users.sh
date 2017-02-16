fb6c5dd9595da4d77f2289a9de90443158441cce
J0lly <jos.ferraris@gmail.com>
2017-02-16 09:43:23 +0100

    updated script import_users

--- files/import_users.sh
+++ files/import_users.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Specify an IAM group for users who should be given sudo privileges, or leave
+# empty to not change sudo access, or give it the value '##ALL##' to have all
+# users be given sudo rights.
+SudoersGroup=""
+[[ -z "${SudoersGroup}" ]] || [[ "${SudoersGroup}" == "##ALL##" ]] || Sudoers=$(
+  aws iam get-group --group-name "${SudoersGroup}" --query "Users[].[UserName]" --output text
+);
+
 aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
   SaveUserName="$User"
   SaveUserName=${SaveUserName//"+"/".plus."}
@@ -5,9 +15,19 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//","/".comma."}
   SaveUserName=${SaveUserName//"@"/".at."}
   if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
-    # sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
     /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName"
+  fi
+
+  if [[ ! -z "${SudoersGroup}" ]]; then
+    # sudo will read each file in /etc/sudoers.d, skipping file names that end
+    # in ‘~’ or contain a ‘.’ character to avoid causing problems with package
+    # manager or editor temporary/backup files.
     SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
-    echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
+    SaveUserSudoFilePath="/etc/sudoers.d/$SaveUserFileName"
+    if [[ "${SudoersGroup}" == "##ALL##" ]] || echo "$Sudoers" | grep "^$User\$" > /dev/null; then
+      echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "$SaveUserSudoFilePath"
+    else
+      [[ ! -f "$SaveUserSudoFilePath" ]] || rm "$SaveUserSudoFilePath"
+    fi
   fi
 done
